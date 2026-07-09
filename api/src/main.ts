29671115fd49391e709f4af14a205ba2532f1c5c
Jos467 <rubiojosecarlos467@gmail.com>
2026-07-08 23:53:27 -0600

    fix: actualizar origen de CORS para Vercel

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://coworking-web-eight.vercel.app'],
   });
 
   app.useGlobalPipes(
